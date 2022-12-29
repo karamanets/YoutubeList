a5d8693014f9bd71a6c1b4be62aeb5f9d37d89ae
Alex Karamanets <alexander.fromworld@gmail.com>
2022-12-29 15:36:53 +0100

    YouTube simple list with descriptions

---
+++ YoutubeList/YoutubeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeListApp.swift
+//  YoutubeList
+//
+//  Created by Alex Karamanets on 26.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Main()
+        }
+    }
+}
